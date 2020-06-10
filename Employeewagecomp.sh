7a9e5227b6a3cd82c3bdec1a036cc7b49b7e2bf8
manojsid <siddammanoj1998@gmail.com>
2020-06-10 15:20:31 +0530

    UC-5: Total Number of days in the month

--- Employeewagecomp.sh
+++ Employeewagecomp.sh
@@ -1,18 +1,37 @@
-#!/bin/bash -x
+#!/bin/bash
 
-isPartTime=1;
-isFullTime=2;
-employeeRateHr=20;
+isPresent=1;
+isAbsent=2;
+wagePerHr=20;
+perDayHr=8;
+Max_working_days=20;
+Max_working_Hrs=100;
 
-checkValue=$((RANDOM%3))
+for (( days = 0; days <= $Max_working_days; days++ )); do
+checkattendence=$((RANDOM%3))
+                
+        case $checkattendence in
+                 1)
+                                echo "Employee is Working as a full time"
+                                WorkingHrs=8;
+                                Salary=$(($wagePerHr * $WorkingHrs))
+                                echo "Full time Employee Salary is: $Salary"
+                                ;;
+                                
+                2)
+                                echo "Employee is working as part time"
+                                WorkingHrs=4;
+                                Salary=$(($wagePerHr * $WorkingHrs))
+                                echo "Part time Employee Salary is: $Salary"
+                                ;;
+                0)
+                                echo "Employee is absent"
+                                WorkingHrs=0;
+                                Salary=0;
 
-case $checkValue in
-	$isPartTime)
-	 hrs=4;;
-	$isFullTime)
-	 hrs=8;;
-	*)
-	hrs=0;;
-esac
-Salary=$(($hrs * $employeeRateHr))
 
+        esac
+
+
+        #statements
+done
