016d8e1d57f212d83df8fa52a3817e17e1eed0b0
manojsid <siddammanoj1998@gmail.com>
2020-06-10 09:53:38 +0530

    Employee presnt or absent

--- Employeewagecomp.sh
+++ Employeewagecomp.sh
@@ -1,2 +1,11 @@
-#!/bin/bash
-echo "Hello employee"
+#!/bin/bash -x
+
+isPresent=0;
+randomcheck=$((RANDOM%2))
+if [[ $randomcheck -eq $ispresent ]];
+then
+	echo "Employee is present"
+else
+	echo "Employee is absent"
+fi
+
