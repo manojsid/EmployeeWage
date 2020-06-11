71144d4abf3bcf6828b0dcf58da5c1307dbe496f
manojsid <siddammanoj1998@gmail.com>
2020-06-11 11:49:30 +0530

    UC6: No of hours added

--- Employeewagecomp.sh
+++ Employeewagecomp.sh
@@ -6,31 +6,48 @@ wagePerHr=20;
 perDayHr=8;
 Max_working_days=20;
 Max_working_Hrs=100;
+TotalWorkingHrs=0;
+WorkingHrsPerDay=0;
+TotalSalary=0;
 
 for (( days = 0; days <= $Max_working_days; days++ )); do
+
+    if [[ $TotalWorkingHrs -le 100  ]]; then
+        #statements
+    
 checkattendence=$((RANDOM%3))
                 
         case $checkattendence in
                  1)
-                                echo "Employee is Working as a full time"
+                                echo "Employee is Working as a full time--- "
                                 WorkingHrs=8;
                                 Salary=$(($wagePerHr * $WorkingHrs))
-                                echo -e "Full time Employee Salary is: $Salary"
+                                echo "Full time Employee Salary is: $Salary"
+                                #echo " ------------------------------------ "
                                 ;;
                                 
+                                
                 2)
-                                echo "Employee is working as part time"
+                                echo "Employee is working as part time -----"
                                 WorkingHrs=4;
                                 Salary=$(($wagePerHr * $WorkingHrs))
-                                echo -e "Part time Employee Salary is: $Salary"
+                                echo "Part time Employee Salary is: $Salary"
+                                #echo " ------------------------------------ "
                                 ;;
+                                
                 0)
-                                echo "Employee is absent"
+                                echo "Employee is absent-------"
                                 WorkingHrs=0;
                                 Salary=0;
-                                ;;
+                                #echo " ----------------------------------- "
 
+                                ;;
+                                
         esac
+                            TotalSalary=$(($TotalSalary + $Salary))
+                            echo "TotalSalary till now is: $TotalSalary -------"
+    fi
+
 
 
         #statements
